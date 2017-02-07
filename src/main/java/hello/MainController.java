e62f6be30c864111d2988557d03f4b991e48ff74
JorgeZapa <kouuke9@gmail.com>
2017-02-07 19:20:18 +0100

    login.html created using html5 with BootStrap and accesible from the Spring Boot project.

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -4,21 +4,42 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.User;
 
 @Controller
 public class MainController {
 
+	//The first page shown will be login.html.
     @RequestMapping("/")
     public String landing(Model model) {
     	model.addAttribute("nombre", "Amigo");
-        return "saludo";
+        return "login";
     }
     
+    //This one only works as manual testing.
     @RequestMapping("/hola")
     public String hola(Model model) {
     	model.addAttribute("nombre", "Luis");
         return "saludo";
+    }
+    
+    //This method process an POST html request once fulfilled the login.html form (clicking in the "Enter" button).
+    @RequestMapping(value="/info" ,method = RequestMethod.POST)
+    public String userOk(Model model, @RequestParam(value="name") String name,
+    		@RequestParam(value="password") String password){
+    	model.addAttribute("nombre", name);
+    	model.addAttribute("pass", password);
+    	return "saludo"; //It should lead to an html file that shows in a user-friendly mode the ifnromation about the user logged.
+    }
+    
+    //This method processes an POST html request when the "Register" button in the login.html form is clicked.
+    @RequestMapping(value="/register", method =RequestMethod.POST)
+    public String letRegister(){
+    	return "register"; //It just shows a simple .html page to be modified.
     }
 
 }
